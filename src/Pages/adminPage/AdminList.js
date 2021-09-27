c30dfd81991faa04c6b2aad6338fe21dc72b1929
ujoo0408 <ujoo0408@naver.com>
2021-09-28 03:34:27 +0900

    와인 전체 리스트 불러오기 및 페이징

--- src/Pages/adminPage/AdminList.js
+++ src/Pages/adminPage/AdminList.js
@@ -13,8 +13,10 @@ import {
   CTableFoot,
   CButton,
 } from "@coreui/react";
+import Pagination from "react-js-pagination"; // install 해주어야 함.
 import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import Style from './Paging.css';
 
 function AdminList() {
   const style = {
@@ -23,22 +25,51 @@ function AdminList() {
     marginRight: "10px",
     float:"right"
   };
+  const nodataStyle = {
+    paddingTop: "10px",
+    height: "35px"
+  };
 
-  const [wineList, setWineList] = useState([{}]);
+  const [wineList, setWineList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const limit = 10;
+  const pageNum = [];
+  
+  const handlePageChange = (page) => { 
+    setPage(page-1); 
+  };
 
   useEffect(() => {
     fetch(
-      `http://ec2-3-35-222-71.ap-northeast-2.compute.amazonaws.com:8082/api/v1/wine`
+      `http://ec2-13-125-218-253.ap-northeast-2.compute.amazonaws.com:8082//api/v1/wine?sort=id,desc&size=10&isPublic=true&page=${page}`
     )
-      .then((res) => res.json())
-      .then((res) => console.log(res.data));
-  }, []);
+      .then((res) => {return res.json();})
+      .then((data) => {
+        console.log(data)
+        if(data.result){
+          setWineList(data.data.wineList);
+          console.log('wineList : '+ wineList);
+          console.log('totalPages'+data.data.totalPages);
+          for(let i = 1 ; i <= data.data.totalPages ; i++) {
+            pageNum.push(i);
+          }
+          setTotal(data.data.totalElements);
+          console.log(pageNum)
+          //setPageCnt(data.data.totalPages);
+        }else{
+          console.log('false');
+        }
+        
+
+      });
+  }, [page]);
 
   return (
     <div>
       <Header>Wineasy Admin-와인목록</Header>
       <CCard className="mb-4">
-        <Link to="./registWine"> 
+        <Link to="./admin/registWine"> 
             <CButton style={style}> 와인 등록하기 </CButton>
         </Link>
         <CCardHeader>WINE LIST</CCardHeader>
@@ -49,60 +80,25 @@ function AdminList() {
                 <CTableHeaderCell className="text-center">Wine No</CTableHeaderCell>
                 <CTableHeaderCell className="text-center">Wine Name</CTableHeaderCell>
                 <CTableHeaderCell className="text-center">Type</CTableHeaderCell>
-                <CTableHeaderCell className="text-center">Acidity</CTableHeaderCell>
-                <CTableHeaderCell className="text-center">Sweet</CTableHeaderCell>
-                <CTableHeaderCell className="text-center">Body</CTableHeaderCell>
-                <CTableHeaderCell className="text-center">Tannin</CTableHeaderCell>
+		            <CTableHeaderCell className="text-center">Wine Variety Name</CTableHeaderCell>
               </CTableRow>
             </CTableHead>
             <CTableBody>
-              <CTableRow>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-                <CTableDataCell className="text-center">A</CTableDataCell>
-                <CTableDataCell className="text-center">red</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">3</CTableDataCell>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-              </CTableRow>
-              <CTableRow>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">B</CTableDataCell>
-                <CTableDataCell className="text-center">white</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-              </CTableRow>
-              <CTableRow>
-                <CTableDataCell className="text-center">3</CTableDataCell>
-                <CTableDataCell className="text-center">C</CTableDataCell>
-                <CTableDataCell className="text-center">rose</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-              </CTableRow>
-              <CTableRow>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-                <CTableDataCell className="text-center">D</CTableDataCell>
-                <CTableDataCell className="text-center">red</CTableDataCell>
-                <CTableDataCell className="text-center">2</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-                <CTableDataCell className="text-center">3</CTableDataCell>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-              </CTableRow>
-              <CTableRow>
-                <CTableDataCell className="text-center">5</CTableDataCell>
-                <CTableDataCell className="text-center">E</CTableDataCell>
-                <CTableDataCell className="text-center">Sparkling</CTableDataCell>
-                <CTableDataCell className="text-center">3</CTableDataCell>
-                <CTableDataCell className="text-center">4</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-                <CTableDataCell className="text-center">1</CTableDataCell>
-              </CTableRow>
-            </CTableBody>
+              { total === 0
+                ? <CTableDataCell className="text-center" colSpan='4' style={nodataStyle}> 데이터가 존재하지 않습니다.</CTableDataCell>
+                :
+                wineList.map(wine => (
+                  <CTableRow key={wine.id}>
+                    <CTableDataCell className="text-center"><Link to={`./admin/registWine?wineNo=${wine.id}`}>{wine.id}</Link></CTableDataCell>
+                    <CTableDataCell className="text-center"><Link to={`./admin/registWine?wineNo=${wine.id}`}>{wine.wineNameKr}</Link></CTableDataCell>
+                    <CTableDataCell className="text-center"><Link to={`./admin/registWine?wineNo=${wine.id}`}>{wine.wineType}</Link></CTableDataCell>
+                    <CTableDataCell className="text-center"><Link to={`./admin/registWine?wineNo=${wine.id}`}>{wine.wineVarietyName}</Link></CTableDataCell>
+                  </CTableRow>
+                ))
+              }
+             </CTableBody>
           </CTable>
+          <Pagination activePage={page+1} itemsCountPerPage={10} totalItemsCount={total} pageRangeDisplayed={10} prevPageText={"‹"} nextPageText={"›"} onChange={handlePageChange} />
         </CCardBody>
       </CCard>
     </div>
--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AdminRegist from "./Pages/adminPage/AdminRegist";
-import AdminBoard from "./Pages/adminPage/AdminBoard";
 import AdminList from "./Pages/adminPage/AdminList";
 
 class Routes extends React.Component {
@@ -9,9 +8,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/admin" component={AdminBoard} />
+          <Route exact path="/admin" component={AdminList} />
           <Route exact path="/admin/registWine" component={AdminRegist} />
-          <Route exact path="/admin/listWine" component={AdminList} />
         </Switch>
       </Router>
     );
