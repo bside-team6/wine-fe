61bc5eccf452dbc753fe12800bdc4c6bfe4f8942
yrnana <nyryn0945@gmail.com>
2021-10-17 22:28:29 +0900

    fix: 포맷팅 및 전반적인 import 수정

--- src/Components/MutliSelect.js
+++ src/Components/MutliSelect.js
@@ -1,12 +1,12 @@
-import React, { useState, useCallback } from "react";
-import Creatable from "react-select/creatable";
+import React from 'react';
+import Creatable from 'react-select/creatable';
 
 const aquaticCreatures = [
-  { label: "롯데마트", value: "롯데마트" },
-  { label: "이마트", value: "이마트" },
-  { label: "홈플러스", value: "홈플러스" },
-  { label: "코스트코", value: "코스트코" },
-  { label: "트레이더스", value: "트레이더스" },
+  { label: '롯데마트', value: '롯데마트' },
+  { label: '이마트', value: '이마트' },
+  { label: '홈플러스', value: '홈플러스' },
+  { label: '코스트코', value: '코스트코' },
+  { label: '트레이더스', value: '트레이더스' },
 ];
 
 function MultiSelect(props) {
--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AdminRegist from "./Pages/adminPage/AdminRegist";
-import AdminList from "./Pages/adminPage/AdminList";
-import AdminDetail from "./Pages/adminPage/AdminDetail";
+import loadable from '@loadable/component';
 
-class Routes extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/admin/detail/:id" component={AdminDetail} />
-          <Route exact path="/admin" component={AdminList} />
-          <Route exact path="/admin/regist" component={AdminRegist} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const AdminPage = loadable(() => import('./pages/admin'));
+
+const Routes = {
+  Admin: AdminPage,
+};
 
 export default Routes;
