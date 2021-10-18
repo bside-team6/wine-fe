b46ddc9149e6c305db4254f6027c2bc1e104eb6b
yrnana <nyryn0945@gmail.com>
2021-10-18 18:36:30 +0900

    feat: 임시 라우트 추가

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import loadable from '@loadable/component';
 const AdminPage = loadable(() => import('./pages/Admin'));
 
 const Routes = {
+  Home: <div>Home (// TODO)</div>,
   Admin: AdminPage,
 };
 
