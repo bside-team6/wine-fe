063fd1d8787dd137b9bf1fbd27fe34812abb19d6
yrnana <nyryn0945@gmail.com>
2021-10-22 07:38:07 +0900

    fix: 코드 포맷팅 적용

--- src/pages/Main/KakaoCallback.js
+++ src/pages/Main/KakaoCallback.js
@@ -22,7 +22,7 @@ function KakaoCallback() {
   //TO-DO : 로그인 , 회원가입 화면 나눌지 체크하기
 
   const current = decodeURI(window.location.href);
-  const search = current.split("?")[1];
+  const search = current.split('?')[1];
   const params = new URLSearchParams(search);
   const code = params.get('code');
   const [nickName, setNickName] = useState('');
@@ -30,9 +30,11 @@ function KakaoCallback() {
   console.log('code : ', code);
   //TO-DO: 가입 링크 404 해결
   //TO-DO: 닉네임 다음에,확인 시 동작 확인
-  const { data } = useQuery(['kakao-signUp', code], () => kakaoSignUp(code), {
-
-  });
+  const { data } = useQuery(
+    ['kakao-signUp', code],
+    () => kakaoSignUp(code),
+    {},
+  );
 
   const handleInput = (e) => {
     //닉네임 입력감 세팅
@@ -41,45 +43,47 @@ function KakaoCallback() {
   };
 
   return (
-  <div >
+    <div>
       <div
         css={(theme) => css`
-            font-family: Noto Sans KR;
-            font-size: 32px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: 46px;
-            letter-spacing: 0em;
-            text-align: center;
-            display: block;
-            margin: 0px auto;
-            height: 46px;
-            width: 89px;
-            left: 916px;
-            top: 241px;
-            border-radius: 0;
-            margin-bottom: 0;
+          font-family: Noto Sans KR;
+          font-size: 32px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 46px;
+          letter-spacing: 0em;
+          text-align: center;
+          display: block;
+          margin: 0px auto;
+          height: 46px;
+          width: 89px;
+          left: 916px;
+          top: 241px;
+          border-radius: 0;
+          margin-bottom: 0;
         `}
-      > 닉네임</div>
+      >
+        닉네임
+      </div>
       <div
         css={(theme) => css`
-            font-family: Noto Sans KR;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 400;
-            color: rgba(168, 168, 168, 1);
-            line-height: 26.06px;
-            letter-spacing: 0em;
-            text-align: center;
-            margin-top: 52px;
-
+          font-family: Noto Sans KR;
+          font-size: 18px;
+          font-style: normal;
+          font-weight: 400;
+          color: rgba(168, 168, 168, 1);
+          line-height: 26.06px;
+          letter-spacing: 0em;
+          text-align: center;
+          margin-top: 52px;
         `}
-      >축하합니다! 가입이 완료 되었습니다.
+      >
+        축하합니다! 가입이 완료 되었습니다.
         <br />
         와인이지에서 쓰실 닉네임을 정해보세요.
       </div>
       <div
-       css={(theme) => css`
+        css={(theme) => css`
           display: block;
           margin: 0px auto;
           margin-top: 24px;
@@ -88,7 +92,8 @@ function KakaoCallback() {
           left: 744px;
           top: 415px;
           border-radius: 8px;
-      `}>
+        `}
+      >
         <input
           css={(theme) => css`
             font-size: 18px;
@@ -100,18 +105,22 @@ function KakaoCallback() {
             left: 744px;
             top: 415px;
             border-radius: 8px;
-        `} type="text"
-           placeholder="닉네임"
-           id="nickName"
-           onChange={handleInput}></input>
+          `}
+          type="text"
+          placeholder="닉네임"
+          id="nickName"
+          onChange={handleInput}
+        ></input>
         <span
-            css={(theme) => css`
-              font-family: Noto Sans KR;
-              font-size: 14px;
-              font-style: normal;
-              font-weight: 400;
-              color: rgba(168, 168, 168, 1);
-        `}>2-16자 국문/영문 대소문자/숫자
+          css={(theme) => css`
+            font-family: Noto Sans KR;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+            color: rgba(168, 168, 168, 1);
+          `}
+        >
+          2-16자 국문/영문 대소문자/숫자
         </span>
       </div>
       <div
@@ -120,30 +129,35 @@ function KakaoCallback() {
           margin: 0px auto;
           text-align: center;
           margin-top: 2px;
-      `}>
-        <button
-            css={(theme) => css`
-              height: 60px;
-              width: 212px;
-              left: 744px;
-              top: 522px;
-              border-radius: 8px;
-              border: 1px solid rgba(197, 197, 197, 1);
-              color: rgba(66, 66, 66, 1);
-              background-color:rgba(255,255,255, 0.3);
-;
-        `}>다음에</button>
+        `}
+      >
         <button
           css={(theme) => css`
-              height: 60px;
-              width: 212px;
-              left: 964px;
-              top: 522px;
-              border-radius: 8px;
-              color:rgba(255,255,255, 1.0);
-              background-color: rgba(0, 0, 0, 1.0);
-
-        `}>확인</button>
+            height: 60px;
+            width: 212px;
+            left: 744px;
+            top: 522px;
+            border-radius: 8px;
+            border: 1px solid rgba(197, 197, 197, 1);
+            color: rgba(66, 66, 66, 1);
+            background-color: rgba(255, 255, 255, 0.3); ;
+          `}
+        >
+          다음에
+        </button>
+        <button
+          css={(theme) => css`
+            height: 60px;
+            width: 212px;
+            left: 964px;
+            top: 522px;
+            border-radius: 8px;
+            color: rgba(255, 255, 255, 1);
+            background-color: rgba(0, 0, 0, 1);
+          `}
+        >
+          확인
+        </button>
       </div>
     </div>
   );
--- src/pages/Main/Login.js
+++ src/pages/Main/Login.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { spritesStyle, alignCenter } from 'styles/common';
 import { KAKAO_AUTH_URL } from 'helpers/oauth';
 import kakaoLoginImg from 'assets/kakao_login_large_wide.png';
 
@@ -20,47 +19,50 @@ const styleInfo = {
 };
 
 function Login() {
-  console.log("ddddd : " , `${KAKAO_AUTH_URL}`);
+  console.log('ddddd : ', `${KAKAO_AUTH_URL}`);
   return (
     //현재 그려지는 화면이 없음
-    <div >
+    <div>
       <div
         css={(theme) => css`
-            font-family: Noto Sans KR;
-            font-size: 32px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: 46px;
-            letter-spacing: 0em;
-            text-align: center;
-            display: block;
-            margin: 0px auto;
-            height: 46px;
-            width: 89px;
-            left: 916px;
-            top: 241px;
-            border-radius: 0;
-            margin-bottom: 0;
+          font-family: Noto Sans KR;
+          font-size: 32px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 46px;
+          letter-spacing: 0em;
+          text-align: center;
+          display: block;
+          margin: 0px auto;
+          height: 46px;
+          width: 89px;
+          left: 916px;
+          top: 241px;
+          border-radius: 0;
+          margin-bottom: 0;
         `}
-      > 로그인</div>
+      >
+        로그인
+      </div>
       <div
         css={(theme) => css`
-            font-family: Noto Sans KR;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 26px;
-            letter-spacing: 0em;
-            text-align: center;
-            margin-top: 52px;
+          font-family: Noto Sans KR;
+          font-size: 18px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 26px;
+          letter-spacing: 0em;
+          text-align: center;
+          margin-top: 52px;
         `}
-      >아이디, 비밀번호 입력하기 귀찮으시죠?
+      >
+        아이디, 비밀번호 입력하기 귀찮으시죠?
         <br />
         카카오로 간편하고 빠르게 로그인 하세요
       </div>
 
       <div
-       css={(theme) => css`
+        css={(theme) => css`
           height: 60px;
           width: 432px;
           left: 744px;
@@ -68,12 +70,13 @@ function Login() {
           display: block;
           margin: 0px auto;
           margin-top: 24px;
-      `}>
-      <img
-        src={kakaoLoginImg}
-        alt="kakaoLoginImg"
-        onClick={() => window.open(`${KAKAO_AUTH_URL}`)}
-      />
+        `}
+      >
+        <img
+          src={kakaoLoginImg}
+          alt="kakaoLoginImg"
+          onClick={() => window.open(`${KAKAO_AUTH_URL}`)}
+        />
       </div>
     </div>
   );
