2ec7253b7ae94f6dd598805f699daed2fd21364f
songhee <realsong88@gmail.com>
2021-10-23 20:59:51 +0900

    WIP

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,11 +2,12 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import KakaoCallback from './KakaoCallback';
 import Signup from './Signup';
-
+import MainSearchBar from './MainSearchBar';
 function Main() {
   return (
     <Switch>
-      <Route exact path="/" component={() => <div>Home</div>} />
+      <Route exact path="/" component={MainSearchBar} />
+      {/* <Route exact path="/" component={() => <div>Home</div>} /> */}
       <Route path="/login" component={Login} />
       <Route path="/kakaoCallback" component={KakaoCallback} />
       <Route path="/signup/:token" component={Signup} />
