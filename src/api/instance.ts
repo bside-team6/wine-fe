aa8cc8e5aaeb559b45f295d61705326e741c52ff
yrnana <nyryn0945@gmail.com>
2021-11-08 08:09:31 +0900

    feat: auth 관련 api 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -5,3 +5,11 @@ const instance = axios.create({
 });
 
 export default instance;
+
+export const kakaoAuth = axios.create({
+  baseURL: 'https://kauth.kakao.com',
+});
+
+export const kakaoApi = axios.create({
+  baseURL: 'https://kapi.kakao.com',
+});
