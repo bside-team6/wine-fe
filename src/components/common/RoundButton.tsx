5704e2941c917f085131c8a3020a2d975e871cdf
yrnana <nyryn0945@gmail.com>
2021-11-11 23:45:24 +0900

    fix: RoundButton 수정

--- src/components/common/RoundButton.tsx
+++ src/components/common/RoundButton.tsx
@@ -1,24 +1,20 @@
 import { css, Theme } from '@emotion/react';
-import { spritesStyle } from '~/styles/common';
+import Icon, { IconName } from '~/components/common/Icon';
 
-const dropdownStyle = css`
-  padding-right: 40px;
-  &:after {
-    position: absolute;
-    top: 6px;
-    right: 14px;
-    content: ' ';
-    ${spritesStyle}
-    background-position: -436px 0px;
-    translate: transform 200ms;
-  }
-`;
+type IconPosition = 'left' | 'right';
+type Size = 'small' | 'medium' | 'large';
 
-const dropdownCloseStyle = css`
-  &:after {
-    transform: rotate(180deg);
-  }
-`;
+const heightMap: Record<Size, number> = {
+  small: 40,
+  medium: 44,
+  large: 52,
+};
+
+const radiusMap: Record<Size, number> = {
+  small: 20,
+  medium: 22,
+  large: 28,
+};
 
 const inactiveStyle = (theme: Theme) => css`
   color: ${theme.colors.black06};
@@ -30,46 +26,82 @@ export interface RoundButtonProps
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   > {
+  variant?: 'contained' | 'outlined';
   color?: 'primary' | 'secondary';
-  dropdown?: 'open' | 'close';
+  size?: 'small' | 'medium' | 'large';
+  bold?: boolean;
+  icon?: IconName;
+  iconPosition?: IconPosition;
   inactive?: boolean;
 }
 
 const RoundButton: React.FC<RoundButtonProps> = ({
   children,
+  variant = 'contained',
   color = 'primary',
-  dropdown,
-  inactive,
+  size = 'medium',
+  bold = true,
+  icon,
+  iconPosition = 'left',
+  inactive = false,
   ...restProps
 }) => {
   return (
     <button
       css={(theme: Theme) => css`
         cursor: pointer;
+        outline: none;
         position: relative;
-        font-weight: bold;
-        padding: 11px 20px;
-        background: #ffffff;
-        border-radius: 22px;
-        border: 2px solid;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        appearance: none;
+        padding: 0 ${children ? 20 : 16}px;
+        font-size: 14px;
+        font-weight: ${bold ? 'bold' : 'normal'};
+        border-radius: ${radiusMap[size]}px;
+        height: ${heightMap[size]}px;
+        ${size === 'large' &&
+        css`
+          min-width: 320px;
+        `}
+        background: ${variant === 'contained'
+          ? color === 'primary'
+            ? theme.colors.main.primary
+            : theme.colors.black07
+          : theme.colors.white};
+        border: ${variant === 'contained' ? 'none' : '2px solid'};
         border-color: ${color === 'primary'
           ? theme.colors.main.primary
-          : theme.colors.black08};
-        color: ${color === 'primary'
+          : theme.colors.black07};
+        color: ${variant === 'contained'
+          ? theme.colors.white
+          : color === 'primary'
           ? theme.colors.main.primary
           : theme.colors.black02};
-        ${dropdown && dropdownStyle}
-        ${dropdown === 'close' && dropdownCloseStyle}
+        ${inactive && inactiveStyle(theme)}
+        svg {
+          &.left-icon {
+            margin-right: ${children ? 8 : 0}px;
+          }
+          &.right-icon {
+            margin-left: 8px;
+          }
+        }
         &:disabled {
           cursor: not-allowed;
-          color: ${theme.colors.black06};
-          border-color: ${theme.colors.black08};
+          ${inactiveStyle(theme)}
         }
-        ${inactive && inactiveStyle(theme)}
       `}
       {...restProps}
     >
+      {icon && iconPosition === 'left' && (
+        <Icon name={icon} className="left-icon" />
+      )}
       {children}
+      {icon && iconPosition === 'right' && (
+        <Icon name={icon} className="right-icon" />
+      )}
     </button>
   );
 };
