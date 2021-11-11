7e9f1fa7d42c8a7751f4204fc20219727d5ac900
yrnana <nyryn0945@gmail.com>
2021-11-12 08:26:00 +0900

    feat: Confirm 컴포넌트 작성

---
+++ src/components/common/Confirm.stories.tsx
@@ -0,0 +1,20 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import Confirm from './Confirm';
+
+export default {
+  title: 'components/Confirm',
+  component: Confirm,
+  args: {
+    isOpen: true,
+    title: '알림',
+    content: `페이지를 이동하면\n작성중인 데이터는 저장되지 않습니다.`,
+    ok: '확인하고 이동',
+    cancel: '취소',
+  },
+} as ComponentMeta<typeof Confirm>;
+
+const Template: ComponentStory<typeof Confirm> = (args) => (
+  <Confirm {...args} />
+);
+
+export const Default = Template.bind({});
