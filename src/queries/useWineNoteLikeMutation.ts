c96aa54183e79b2085ee651edf0311742e621edb
yrnana <nyryn0945@gmail.com>
2021-11-15 07:31:30 +0900

    feat: 좋아요 기능 낙관적 업데이트

--- src/queries/useWineNoteLikeMutation.ts
+++ src/queries/useWineNoteLikeMutation.ts
@@ -1,18 +1,60 @@
 import type { AxiosError } from 'axios';
-import { useMutation } from 'react-query';
+import { produce } from 'immer';
+import { InfiniteData, useMutation, useQueryClient } from 'react-query';
 import { toggleWineNoteLike } from '~/api/wine-note';
-import type { MutationOptions } from '~/types';
+import type {
+  IPageable,
+  IWineNote,
+  IWineNoteDetail,
+  MutationOptions,
+} from '~/types';
 
 const useWineNoteLikeMutation = (
   options?: MutationOptions<unknown, AxiosError, number>,
 ) => {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
   return useMutation<unknown, AxiosError, number>(toggleWineNoteLike, {
     ...options,
     onMutate: (wineNoteId) => {
       // Optimistic Updates
-      // queryClient.setQueryData('todos', old => [...old, newTodo])
+
+      // 와인노트 목록 `isLike` 토글
+      queryClient.setQueriesData<InfiniteData<IPageable<IWineNote>>>(
+        ['wine-notes'],
+        produce((draft) => {
+          if (draft?.pages?.length) {
+            for (let i = 0; i < draft.pages.length; i++) {
+              const idx = draft.pages[i].content.findIndex(
+                (note) => note.id === wineNoteId,
+              );
+              if (idx !== -1) {
+                draft.pages[i].content[idx].isLike =
+                  !draft.pages[i].content[idx].isLike;
+              }
+            }
+          }
+        }),
+      );
+      // 상세와인노트 `isLike` 토글
+      queryClient.setQueriesData<IWineNoteDetail>(
+        ['wine-note'],
+        produce((draft) => {
+          if (draft?.id === wineNoteId) {
+            draft.isLike = !draft.isLike;
+          }
+        }),
+      );
+      // 관련와인노트 `isLike` 토글
+      queryClient.setQueryData<IWineNote[]>(
+        ['related-wine-notes'],
+        produce((draft) => {
+          const idx = draft?.findIndex((note) => note.id === wineNoteId);
+          if (draft && idx && idx !== -1) {
+            draft[idx].isLike = !draft[idx].isLike;
+          }
+        }),
+      );
     },
   });
 };
