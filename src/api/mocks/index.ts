97e481ed303d3280062e857a17e27307686ffa4d
yrnana <nyryn0945@gmail.com>
2021-11-15 00:04:08 +0900

    feat: mock 삭제

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,10 +1,3 @@
 import { setupWorker } from 'msw';
-import {
-  getRelatedWineNotesSuccessHandler,
-  getWineNotesHandler,
-} from '~/api/mocks/wine-note';
 
-export const worker = setupWorker(
-  getWineNotesHandler,
-  getRelatedWineNotesSuccessHandler,
-);
+export const worker = setupWorker();
