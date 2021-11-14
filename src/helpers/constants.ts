e05f4c94c7345f4abe091c0b7c7759d9598d61e1
yrnana <nyryn0945@gmail.com>
2021-11-15 00:40:53 +0900

    fix: https로 변경

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = isProduction
-  ? 'http://www.wineasy.kr'
+  ? 'https://www.wineasy.kr'
   : 'http://localhost:3000';
