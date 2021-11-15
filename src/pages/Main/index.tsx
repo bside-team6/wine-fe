9d74595e0948f70ba20270b09cdd0f92092ba923
yrnana <nyryn0945@gmail.com>
2021-11-16 00:05:29 +0900

    feat: 메인 레이아웃 작업

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,13 +3,12 @@ import { GuestOnlyLayout } from '~/components/route/GuestOnly';
 import KakaoCallback from './Auth/KakaoCallback';
 import SignupStep1 from './Auth/SignupStep1';
 import SignupStep2 from './Auth/SignupStep2';
-import MainSearchBar from './MainSearchBar';
+import Home from './Home';
 
-// FIXME: 반영 후 developers 에서 주소 변경
 function Main() {
   return (
     <Routes>
-      <Route index element={<MainSearchBar />} />
+      <Route index element={<Home />} />
       <Route path="signup" element={<GuestOnlyLayout />}>
         <Route path="step1" element={<SignupStep1 />} />
         <Route path="step2" element={<SignupStep2 />} />
